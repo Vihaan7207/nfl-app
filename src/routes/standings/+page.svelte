fecb1cd5b643f2d20aa714b94c54a1dc3839c252
Vihaan7207 <vmikki7207@gmail.com>
2025-06-17 18:17:49 -0400

    init ui library

---
+++ src/routes/standings/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from "svelte";
+    import { PUBLIC_API_KEY } from "$env/static/public";
+
+    let standings;
+    const date = new Date();
+    const year = date.getFullYear();
+
+    let response;
+
+    onMount(async() => {
+        try {
+            response = await fetch(`https://api.sportsdata.io/v3/nfl/scores/json/Standings/2024?key=${PUBLIC_API_KEY}`);
+            standings = await response.json();
+        } catch (error) {
+
+        }
+    });
+</script>
+
+{#if standings}
+    <!-- <pre>{JSON.stringify(standings, null, 2)}</pre> -->
+{:else}
+    <p>Loading standings...</p>
+{/if}
